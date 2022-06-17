561dcb327ed7f4ba4f089fc45ecf8ca0f08f0026
Kuss <kussssss@outlook.com>
2022-06-17 16:27:02 +0800

    chores: coverImgUrl

--- packages/nextra-theme-rewind/src/components/landing.tsx
+++ packages/nextra-theme-rewind/src/components/landing.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { useRouter, NextRouter } from "next/router";
 import cn from 'classnames'
-
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 interface Button {
   content: React.ReactElement;
   type?: 'default' | 'ghost';
   onClick: (router: NextRouter) => void
+  coverImgUrl?: string
 }
 
 interface Props {
@@ -30,43 +32,79 @@ const BTN_GHOST = [
   'dark:border dark:opacity-40 dark:hover:opacity-100',
 ]
 
+const squareVariants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 }
+};
+
 const Landing: FC<Props> = ({
   buttons,
   title,
   subtitle,
-  keywords
+  keywords,
+  coverImgUrl
 }) => {
   const router = useRouter();
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
-    <div className='h-full flex flex-col items-center justify-center'>
-      <div className='mb-8 text-center'>
-        <div className='text-4xl font-bold mb-6'>{title}</div>
-        <div className='text-xl mb-6'>{subtitle}</div>
-        {keywords && keywords.length > 0 &&
-          <div className='flex flex-col'>
-            {keywords.map((keyword, i) => (
-              <span className='font-bold' key={i}>{keyword}</span>
+    <>
+      <div className='h-screen flex flex-col items-center justify-center'>
+        <div className='mb-8 text-center'>
+          <div className='text-4xl font-bold mb-6'>{title}</div>
+          <div className='text-xl mb-6'>{subtitle}</div>
+          {keywords && keywords.length > 0 &&
+            <div className='flex flex-col'>
+              {keywords.map((keyword, i) => (
+                <span className='font-bold' key={i}>{keyword}</span>
+              ))}
+            </div>
+          }
+        </div>
+        {buttons && buttons.length > 0 && (
+          <div className='flex space-x-8'>
+            {buttons.map((btn, i) => (
+              <button
+                key={i}
+                onClick={() => btn.onClick(router)}
+                className={cn(
+                  ...BTN_BASE,
+                  ...(btn.type === 'ghost' ? BTN_GHOST : BTN_DEFAULT)
+                )}
+              >
+                {btn.content}
+              </button>
             ))}
           </div>
-        }
+        )}
       </div>
-      {buttons && buttons.length > 0 && (
-        <div className='flex space-x-8'>
-          {buttons.map((btn, i) => (
-            <button
-              key={i}
-              onClick={() => btn.onClick(router)}
-              className={cn(
-                ...BTN_BASE,
-                ...(btn.type === 'ghost' ? BTN_GHOST : BTN_DEFAULT)
-              )}
-            >
-              {btn.content}
-            </button>
-          ))}
-        </div>
-      )}
-    </div>
+      {coverImgUrl &&
+        <motion.div
+          ref={ref}
+          animate={controls}
+          variants={squareVariants}
+          initial="hidden"
+          className='h-screen'
+        >
+          <div
+            className='flex flex-col items-center justify-center'
+          >
+            <img
+              src={coverImgUrl}
+              width="600px"
+              alt="封面"
+            />
+          </div>
+        </motion.div>
+      }
+    </>
   );
 }
 
