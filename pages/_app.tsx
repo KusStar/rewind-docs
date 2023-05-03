03e1e18bce9fd5ec601847cf0f373aee94407a76
Kuss <kussssss@outlook.com>
2023-05-03 22:12:18 +0800

    feat: remove @vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/base.css'
 import '../styles/custom.css'
 
-import { Analytics } from "@vercel/analytics/react";
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
