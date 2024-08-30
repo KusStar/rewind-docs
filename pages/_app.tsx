9681c0af4e97406563809e69af4607119710b54d
kusstar <kussssss@outlook.com>
2024-08-31 00:28:09 +0800

    feat: replace ga with umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/base.css'
 import '../styles/custom.css'
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from 'next/script'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      <Script defer src="https://umami.uselessthing.top/script.js" data-website-id="rewind-docs"></Script>
       <Component {...pageProps} />
     </>
   );
